56b78ca3aada898ab83ac5f6953a02d827306fde
takumi555 <htaaa838317@gmail.com>
2021-01-20 18:50:30 +0900

    active_storageを使って画像を投稿し、表紙できるようにした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    
   end
 
   def new
@@ -46,7 +47,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :prefecture, :area, :content, :url)
+    params.require(:post).permit(:title, :prefecture, :area, :content, :url, images: [])
   end
 
 end
