4ccee1dc70df9367166ce0fda33f9d7912b0eb69
takumi555 <htaaa838317@gmail.com>
2021-01-20 22:48:43 +0900

    コメント機能の実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
+
+  def new
+    post = Post.find(params[:post_id])
+    @comment = post.comments.build
+
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    @comment = post.comments.build(comment_params.merge!(user_id: current_user.id))
+    if @comment.save!
+      redirect_to post_path(post), notice: 'コメントできました'
+    else
+      flash.now[:error] = 'コメントできませんでした'
+      render :new
+    end
+
+  end
+
+  def destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   validates :prefecture, presence: true
   validates :content, presence: true
 
+  has_many :comments, dependent: :destroy
   belongs_to :user
 
   has_many_attached :images
