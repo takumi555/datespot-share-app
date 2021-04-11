489d54b481c7e93b4ed676ea08917a16538520ca
takumi555 <kato.takumi1998@gmail.com>
2021-04-11 19:37:42 +0900

    .envファイルを作成

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module DatespotShareApp
     config.assets.paths << config.root.join("vendor/assets/javascripts")
     config.assets.paths << config.root.join("vendor/assets/stylesheets")
 
+    Bundler.require(*Rails.groups)
+    Dotenv::Railtie.load
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
