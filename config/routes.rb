8c76e439dd7202322d920d0e59b97f0d9e4b9a41
takumi555 <kato.takumi1998@gmail.com>
2021-04-18 02:48:04 +0900

    フォロー機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,16 +10,20 @@ Rails.application.routes.draw do
     resource :like, only: [:show, :create, :destroy]
   end
 
-  resources :questions, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-    resources :answers, only: [:index, :create, :destroy]
-  end
-  
   resources :favorites, only: [:index]
   resource :profile, only: [:show, :update]
-  resources :accounts, only: [:show]
+
+  resources :accounts, only: [:show] do
+    resources :follows, only: [:create]
+    resources :unfollows, only: [:create]
+  end
 
   resources :populars, only: [:index]
   resources :search, only: [:index]
   resources :tags, only: [:index]
   resources :prefectures, only: [:index, :show]
+
+  resources :questions, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :answers, only: [:index, :create, :destroy]
+  end
 end
