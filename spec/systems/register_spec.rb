c318a2b5effd21faca338f91b5111a7ac9aa0835
takumi555 <kato.takumi1998@gmail.com>
2021-04-29 00:12:23 +0900

    投稿一覧画面のユーザー画像のサイズの修正

--- spec/systems/register_spec.rb
+++ spec/systems/register_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'ユーザー登録' do
+RSpec.describe 'アカウント作成' do
 
   it 'サインアップできる' do
     visit new_user_registration_path
@@ -8,7 +8,7 @@ RSpec.describe 'ユーザー登録' do
     fill_in 'user[email]', with: 'foo@example.com'
     fill_in 'user[password]', with: '123456'
     fill_in 'user[password_confirmation]', with: '123456'
-    click_on 'サインアップ'
+    click_on 'アカウント作成'
     expect(page). to have_css('.post_index_title', text: '投稿一覧')
   end
 end
